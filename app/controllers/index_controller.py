b6da46437389abda8cd6526e6576668b01acf526
Satya Abhilash <Abhi@MSS-MacMini.T-mobile.com>
2022-02-06 11:26:03 -0500

    Updated Name

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "kwilliam"
+        name = "Satya Abhilash"
         return render_template('index.html', name=name)
