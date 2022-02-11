a66697223459c6a9d3376f1c45ee7273368dbe1d
SatyaAbhilash98 <abhilash.dommeti@gmail.com>
2022-02-10 20:37:55 -0500

    Updated webpage

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Keith Williams 3"
+        name = "Satya Abhilash"
         return render_template('index.html', name=name)
